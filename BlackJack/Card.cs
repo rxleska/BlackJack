0416f61943c0d49bc2e28a5123df7938875a5c74
Ryan Leska <45864749+rxleska@users.noreply.github.com>
2019-01-26 22:29:51 -0600

    Made a deck list and a card class, with system to draw a random card then remove it from the list and display all other cards

---
+++ BlackJack/Card.cs
@@ -0,0 +1,34 @@
+﻿namespace BlackJack
+{
+    public class Card
+    {
+        public int NumericValue { get; set; }
+        public int SuitNumeric { get; set; }
+        public string Suit { get; set; }
+        public Card(int numericValue, int suit)
+        {
+            NumericValue = numericValue;
+            SuitNumeric = suit;
+            if (SuitNumeric == 0)
+            {
+                Suit = "Heart";
+            }
+            else if (SuitNumeric == 1)
+            {
+                Suit = "Club";
+            }
+            else if (SuitNumeric == 2)
+            {
+                Suit = "Diamond";
+            }
+            else if (SuitNumeric == 3)
+            {
+                Suit = "Spade";
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ BlackJack/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlackJack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<Card> deck = new List<Card>();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int x = 0; x < 13; x++)
+                {
+                    Card y = new Card(x + 1, i );
+                    deck.Add(y);
+
+                }
+            }
+            Random rnd = new Random();
+            int z = rnd.Next(0, 25);
+
+            
+            Console.WriteLine("{0} {1}", deck[z].Suit, deck[z].NumericValue);
+
+            deck.Remove(deck[z]);
+
+            Console.WriteLine("\n");
+
+            foreach (var card in deck) {
+                Console.WriteLine("{0} {1}", card.Suit, card.NumericValue);
+            }
+
+            Console.ReadLine();
+        }
+    }
+    
+}
